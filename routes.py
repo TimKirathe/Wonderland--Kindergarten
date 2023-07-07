3b99474dadd328cc797322055bbea4e200c8e4a1
timothykirathe <timothy.kirathe@student.moringaschool.com>
2023-07-07 23:39:08 +0300

    Styled the about us and contact us sections

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-from flask import Flask, request, render_template, jsonify, redirect, url_for, session
+from flask import Flask, request, render_template, jsonify, redirect, url_for, session, request
 from flask_session import Session
 
 
@@ -10,9 +10,25 @@ app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
 Session(app)
 
+# @app.before_request
+# def enforce_https():
+#     if request.url.startswith('http://'):
+#         url = request.url.replace('http://', 'https://', 1)
+#         return redirect(url, code=301)
+
 @app.route("/")
+@app.route("/index")
+@app.route("/home")
 def index():
     return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
