0119c2d46ba961e6ecc0ae4effe8955dad35e6b5
timothykirathe <timothy.kirathe@student.moringaschool.com>
2023-07-09 15:28:18 +0300

    Added back-end code for the submission of an enquiry

--- routes.py
+++ routes.py
@@ -7,9 +7,40 @@ from flask_sqlalchemy import SQLAlchemy
 
 load_dotenv()
 app = Flask(__name__, static_folder = 'static')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ.get('DATABASE_TRACK_MODIFICATIONS')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SESSION_PERMANENT'] = False
 Session(app)
+db = SQLAlchemy()
+Session(app)
+
+class ParentEnquiry(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(120), nullable = False)
+    email = db.Column(db.String(256), nullable = False)
+    enquiry = db.Column(db.String, nullable = False)
+
+    def __repr__(self):
+        return f"User: {self.email}"
+    
+db.init_app(app)
+with app.app_context():
+    # User.__table__.drop(db.engine)
+    db.create_all()
+
+# with app.app_context():
+#     delete all users from database
+#     db.session.query(User).delete()
+#     db.session.commit()
+#     delete a specific user from the database
+    # users = User.query.all()
+    # print(users)
+    # user = User.query.filter_by(email="mit@gmail.com").first()
+    # print(user)
+    # db.session.delete(user)
+    # db.session.commit() 
 
 # @app.before_request
 # def enforce_https():
@@ -35,7 +66,20 @@ def contact():
             parent_email = request.form["parentEnquiryEmail"]
             parent_enquiry = request.form["enquiry"]
 
+            enq = ParentEnquiry(name=parent_name, email=parent_email, enquiry=parent_enquiry)
+            db.session.add(enq)
+            db.session.commit()
+
         else:
+            student_first_name = request.form[""]
+            student_last_name = request.form[""]
+            student_first_name = request.form[""]
+            student_first_name = request.form[""]
+            student_first_name = request.form[""]
+            student_first_name = request.form[""]
+            student_first_name = request.form[""]
+            student_first_name = request.form[""]
+            student_first_name = request.form[""]
 
     return render_template("contact.html")
 
